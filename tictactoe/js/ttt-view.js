e71333c4d97f67f7fc468657bf7049082605ab99
Victor Li <victor.li158@gmail.com>
2017-05-01 17:04:19 -0700

    Implement UI (need to add highlighting in win condition)

--- tictactoe/js/ttt-view.js
+++ tictactoe/js/ttt-view.js
@@ -8,25 +8,36 @@ class View {
 
     // use arrow to prevent global this scoping
     $.each($('.cell'), (i, el) => {
-      // console.log($(el));
-      $(el).on("click", (event) => {
-        // alert(`EVENT: ${event.type} | EL CLICKED: ${i}`);
-        console.log(el);
-        console.log($(el).data("pos"));
-        this.game.playMove($(el).data("pos"));
-        $(el).css("background-color", "white");
-      });
+      this.makeMove($(el));
     });
   }
 
-  makeMove($square) {}
+  makeMove($square) {
+    $square.on("click", (event) => {
+      try {
+        const mark = this.game.currentPlayer;
+        this.game.playMove($square.data("pos"));
+        $square.css("background-color", "white");
+        $square.html(mark);
+      } catch(err) {
+        alert(`Error: ${err.msg}`);
+      }
+      console.log(this.game.winner());
+      if (this.game.isOver()) {
+        $('.ttt').append(`<figcaption>You win, ${this.game.winner()}!</figcaption>`);
+        $.each($('.cell'), (i, el) => {
+          $(el).off("click");
+        });
+      }
+    });
+  }
 
   setupBoard() {
     let $list = $('<ul class="grid"></ul>');
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        let cell = this.game.board.grid[i][j];
-        let jCell = $(`<li class="cell">${cell}</li>`);
+        // let cell = this.game.board.grid[i][j];
+        let jCell = $(`<li class="cell"></li>`);
         $list.append(jCell);
         console.log($(jCell).data("pos", [i, j]));
         console.log($(jCell).data("pos"));
